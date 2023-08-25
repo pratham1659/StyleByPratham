d6f9c8a00da259fe2556687adfc1b1d279f6ada4
prathamkumar <prathamkumar1985@gmail.com>
2023-08-26 02:06:54 +0530

    This is my First Commit of the Day

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Navigation from "./customer/components/Navigation/Navigation";
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
