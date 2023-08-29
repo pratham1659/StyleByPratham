beb205fbd5cece885b4b8f729c7c5b51c2072684
prathamkumar <prathamkumar1985@gmail.com>
2023-08-30 00:09:42 +0530

    JWT Token Authentication

---
+++ backend-spring/src/main/java/com/stylebypratham/config/JwtConstant.java
@@ -0,0 +1,8 @@
+package com.stylebypratham.config;
+
+public class JwtConstant {
+    public  static final String SECRET_KEY="hidhiiuasfnbkabfsiajbjhbbaisfubaisfbnjaknfjsnaifbajsfkjni";
+    public static  final String JWT_HEADER="Authorization";
+
+
+}
