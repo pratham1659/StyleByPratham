1988ef1e4ca2142dd3d126f94059c9252b9520ac
prathamkumar <prathamkumar1985@gmail.com>
2023-08-30 22:13:31 +0530

    Model Class Implementation

--- backend-spring/src/main/java/com/stylebypratham/service/UserService.java
+++ backend-spring/src/main/java/com/stylebypratham/service/UserService.java
@@ -1,6 +1,12 @@
 package com.stylebypratham.service;
 
-public class UserService {
+import com.stylebypratham.exception.UserException;
+import com.stylebypratham.model.User;
 
+
+public interface UserService {
+
+    public User findUserById(Long userId) throws UserException;
+    public User findUserProfileByJwt(String jwt) throws UserException;
 
 }
