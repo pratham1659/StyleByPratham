66ca9998d077b78860fb7848b9315b608fe6f351
prathamkumar <prathamkumar1985@gmail.com>
2023-09-11 02:13:21 +0530

    Backend Last Changes

---
+++ backend-spring/src/main/java/com/stylebypratham/api/api.js
@@ -0,0 +1,351 @@
+export const mens_clothing = [
+    {
+        "imageUrl":
+            "https://sslimages.shoppersstop.com/sys-master/images/hdb/he9/29307680849950/S23IMSTSS23003_CHECKS.jpg_230Wx334H",
+        "brand": "Impact",
+        "title": "Checkered Cotton Slim Fit Men's Casual Shirt",
+        "color": "Brown",
+        "discountedPrice": "1999",
+        "actualPrice": "599",
+        "discountPercent": "70",
+        "size": [
+            {
+                "name": "S",
+                "quantity": 20
+            },
+            {
+                "name": "M",
+                "quantity": 20
+            },
+            {
+                "name": "L",
+                "quantity": 20
+            }
+        ],
+        "quantity": 50,
+        "topLevelCategory": "Men",
+        "secondLevelCategory": "clothing",
+        "thirdLevelCategory": "mens_shirt",
+        "description": "This shirt is a must-have in your closet."
+    },
+    {
+        "imageUrl":
+            "https://sslimages.shoppersstop.com/sys-master/images/h85/h41/17255483605022/A21INFUSE30_GREEN.jpg_230Wx334H",
+        "brand": "Infuse",
+        "title": "Printed Rayon Comfort Fit Men's Casual Shirt",
+        "color": "Green",
+        "discountedPrice": "1299",
+        "actualPrice": "389",
+        "discountPercent": "70",
+        "size": [
+            {
+                "name": "S",
+                "quantity": 20
+            },
+            {
+                "name": "M",
+                "quantity": 20
+            },
+            {
+                "name": "L",
+                "quantity": 20
+            }
+        ],
+        "quantity": 50,
+        "topLevelCategory": "Men",
+        "secondLevelCategory": "clothing",
+        "thirdLevelCategory": "mens_shirt",
+        "description": "This shirt is a must-have in your closet."
+    },
+    {
+        "imageUrl":
+            "https://sslimages.shoppersstop.com/sys-master/images/hde/hdc/28891335557150/S23LDDOBBY142NV_NAVY.jpg_230Wx334H",
+        "brand": "Life",
+        "title": "Textured Cotton Slim Fit Mens Shirt",
+        "color": "Navy Blue",
+        "discountedPrice": "699",
+        "actualPrice": "1499",
+        "discountPercent": "53",
+        "size": [
+            {
+                "name": "S",
+                "quantity": 20
+            },
+            {
+                "name": "M",
+                "quantity": 20
+            },
+            {
+                "name": "L",
+                "quantity": 20
+            }
+        ],
+        "quantity": 50,
+        "topLevelCategory": "Men",
+        "secondLevelCategory": "clothing",
+        "thirdLevelCategory": "mens_shirt",
+        "description": "Navy blue, cotton, slim fit shirt by Life. This shirt has button-down detailing at the front, a stiff collar, breast pocket,"
+    },
+    {
+        "imageUrl":
+            "https://sslimages.shoppersstop.com/sys-master/images/h47/h51/29285374656542/S23FTWLCHK197GY_GREY.jpg_230Wx334H",
+        "brand": "Fratini",
+        "title": "Solid Cotton and Slim Fit Mens Shirt",
+        "color": "Grey",
+        "discountedPrice": "699",
+        "actualPrice": "1499",
+        "discountPercent": "53",
+        "size": [
+            {
+                "name": "S",
+                "quantity": 20
+            },
+            {
+                "name": "M",
+                "quantity": 20
+            },
+            {
+                "name": "L",
+                "quantity": 20
+            },
+        ],
+        "quantity": 50,
+        "topLevelCategory": "Men",
+        "secondLevelCategory": "clothing",
+        "thirdLevelCategory": "mens_shirt",
+        "description": "Grey, regular fit, cotton shirt by Fratini. This shirt is a must-have in your closet.",
+    },
+
+    {
+        "imageUrl":
+            "https://sslimages.shoppersstop.com/sys-master/images/h46/h2e/26466133671966/S21182FAGAP11NV_NAVY.jpg_230Wx334H",
+        "brand": "Stop",
+        "title": "Printed Cotton Slim Fit Men's Formal Shirt",
+        "color": "Navy Blue",
+        "discountedPrice": "699",
+        "actualPrice": "1499",
+        "discountPercent": "48",
+        "size": [
+            {
+                "name": "S",
+                "quantity": 20
+            },
+            {
+                "name": "M",
+                "quantity": 20
+            },
+            {
+                "name": "L",
+                "quantity": 20
+            },
+        ],
+        "quantity": 50,
+        "topLevelCategory": "Men",
+        "secondLevelCategory": "clothing",
+        "thirdLevelCategory": "mens_shirt",
+        "description": "."
+    },
+    {
+        "imageUrl":
+            "https://sslimages.shoppersstop.com/sys-master/images/hd0/h82/28968258666526/A22MSH1637_MULTI.jpg_230Wx334H",
+        "brand": "Tistabene",
+        "title": "Printed Cotton Regular Fit Men's Casual Shirt",
+        "color": "White",
+        "discountedPrice": "899",
+        "actualPrice": "2199",
+        "discountPercent": "68",
+        "size": [
+            {
+                "name": "S",
+                "quantity": 20
+            },
+            {
+                "name": "M",
+                "quantity": 20
+            },
+            {
+                "name": "L",
+                "quantity": 20
+            },
+        ],
+        "quantity": 50,
+        "topLevelCategory": "Men",
+        "secondLevelCategory": "clothing",
+        "thirdLevelCategory": "mens_shirt",
+        "description": "."
+    },
+    {
+        "imageUrl":
+            "https://sslimages.shoppersstop.com/sys-master/images/h1b/hda/28890563346462/S23STSTELR98LC_LILAC.jpg_230Wx334H",
+        "brand": "Stop",
+        "title": "Textured Cotton Regular Fit Men's Shirt",
+        "color": "Purple",
+        "discountedPrice": "899",
+        "actualPrice": "2199",
+        "discountPercent": "68",
+        "size": [
+            {
+                "name": "S",
+                "quantity": 20
+            },
+            {
+                "name": "M",
+                "quantity": 20
+            },
+            {
+                "name": "L",
+                "quantity": 20
+            },
+        ],
+        "quantity": 50,
+        "topLevelCategory": "Men",
+        "secondLevelCategory": "clothing",
+        "thirdLevelCategory": "mens_shirt",
+        "description": "."
+    },
+    {
+        "imageUrl":
+            "https://sslimages.shoppersstop.com/sys-master/images/hdd/h8c/28279385194526/A22SFCSSPT97087_OLIVE.jpg_230Wx334H",
+        "brand": "Louis Philippe Jeans",
+        "title": "Solid Cotton Slim Fit Men's Casual Shirt",
+        "color": "Green",
+        "discountedPrice": "899",
+        "actualPrice": "2199",
+        "discountPercent": "68",
+        "size": [
+            {
+                "name": "S",
+                "quantity": 20
+            },
+            {
+                "name": "M",
+                "quantity": 20
+            },
+            {
+                "name": "L",
+                "quantity": 20
+            },
+        ],
+        "quantity": 50,
+        "topLevelCategory": "Men",
+        "secondLevelCategory": "clothing",
+        "thirdLevelCategory": "mens_shirt",
+        "description": "."
+    },
+    {
+        "imageUrl":
+            "https://sslimages.shoppersstop.com/sys-master/images/h8b/hb4/28613393514526/A22SFCSSPU53739_WHITE.jpg_136Wx204H",
+        "brand": "Louis Philippe Jeans",
+        "title": "Solid Cotton Slim Fit Men's Casual Shirt",
+        "color": "White",
+        "discountedPrice": "899",
+        "actualPrice": "2199",
+        "discountPercent": "68",
+        "size": [
+            {
+                "name": "S",
+                "quantity": 20
+            },
+            {
+                "name": "M",
+                "quantity": 20
+            },
+            {
+                "name": "L",
+                "quantity": 20
+            },
+        ],
+        "quantity": 50,
+        "topLevelCategory": "Men",
+        "secondLevelCategory": "clothing",
+        "thirdLevelCategory": "mens_shirt",
+        "description": "."
+    },
+    {
+        "imageUrl":
+            "https://sslimages.shoppersstop.com/sys-master/images/hc9/h67/30131907035166/S23MSH2184_BLACK.jpg_230Wx334H",
+        "brand": "Tistabene",
+        "title": "Floral Crepe Regular Fit Men's Casual Shirt",
+        "color": "White",
+        "discountedPrice": "899",
+        "actualPrice": "2199",
+        "discountPercent": "68",
+        "size": [
+            {
+                "name": "S",
+                "quantity": 20
+            },
+            {
+                "name": "M",
+                "quantity": 20
+            },
+            {
+                "name": "L",
+                "quantity": 20
+            },
+        ],
+        "quantity": 50,
+        "topLevelCategory": "Men",
+        "secondLevelCategory": "clothing",
+        "thirdLevelCategory": "mens_shirt",
+        "description": "."
+    },
+    {
+        "imageUrl":
+            "https://sslimages.shoppersstop.com/sys-master/images/h41/h75/17255670186014/A21INFUSE30_PEACH.jpg_230Wx334H",
+        "brand": "Tistabene",
+        "title": "Floral Crepe Regular Fit Men's Casual Shirt",
+        "color": "Pink",
+        "discountedPrice": "499",
+        "actualPrice": "839",
+        "discountPercent": "68",
+        "size": [
+            {
+                "name": "S",
+                "quantity": 20
+            },
+            {
+                "name": "M",
+                "quantity": 20
+            },
+            {
+                "name": "L",
+                "quantity": 20
+            },
+        ],
+        "quantity": 50,
+        "topLevelCategory": "Men",
+        "secondLevelCategory": "clothing",
+        "thirdLevelCategory": "mens_shirt",
+        "description": "."
+    },
+    {
+        "imageUrl":
+            "https://sslimages.shoppersstop.com/sys-master/images/h40/hf6/29152708821022/S23LCSLTWL162YL_YELLOW.jpg_230Wx334H",
+        "brand": "Tistabene",
+        "title": "Floral Crepe Regular Fit Men's Casual Shirt",
+        "color": "Yellow",
+        "discountedPrice": "799",
+        "actualPrice": "910",
+        "discountPercent": "34",
+        "size": [
+            {
+                "name": "S",
+                "quantity": 20
+            },
+            {
+                "name": "M",
+                "quantity": 20
+            },
+            {
+                "name": "L",
+                "quantity": 20
+            },
+        ],
+        "quantity": 50,
+        "topLevelCategory": "Men",
+        "secondLevelCategory": "clothing",
+        "thirdLevelCategory": "mens_shirt",
+        "description": "."
+    },
+];
