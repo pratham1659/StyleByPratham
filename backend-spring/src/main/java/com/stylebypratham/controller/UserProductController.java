803e576bfb05cc21aa3a0db48829770c8e00a42e
prathamkumar <prathamkumar1985@gmail.com>
2023-09-10 23:14:41 +0530

    Backend Last Changes

--- backend-spring/src/main/java/com/stylebypratham/controller/UserProductController.java
+++ backend-spring/src/main/java/com/stylebypratham/controller/UserProductController.java
@@ -2,6 +2,7 @@ package com.stylebypratham.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +20,7 @@ import com.stylebypratham.service.ProductService;
 @RequestMapping("/api")
 public class UserProductController {
 
+    @Autowired
     private ProductService productService;
 
     public UserProductController(ProductService productService) {
